e0ea0b9efe75da1c0d62008ecfa4120ed59fc623
MarekSarvas <sarvas.sarvas@gmail.com>
2020-03-01 21:59:59 +0100

    working on GET request

--- project1/src/server.py
+++ project1/src/server.py
@@ -1,5 +1,36 @@
 import sys
 import socket
+import re
+
+
+def handleGET(recv_list):
+  #  print("|"+recv_list[1].split("\r\n")[0]+"|")
+    request_head = recv_list[1].split("\r\n")[0]
+    if(re.fullmatch(r"\/resolve\?name=.*&type=(A|PTR) HTTP\/1\.1", request_head) != None):
+        name = request_head.split("name=")[1]
+        name = name.split("&")[0]
+
+        request_type = request_head.split("type=")[1]
+        request_type = request_type.split(" ")[0]
+
+       # print("|"+name+request_type+"|")
+
+        return resolveRequest(request_type, name)
+    else:
+        return b"HTTP/1.1 405 Method Not Allowed\r\n\r\n"
+
+def handlePOST():
+    print("POST")
+    return b"default post\r\n"
+
+def resolveRequest(req_type, req_name):
+    if(req_type == "A"):
+        
+        return b"HTTP/1.1 200 Ok\r\n\r\n"
+    elif(req_type == "PTR"):
+        return b"HTTP/1.1 200 Ok\r\n\r\n"
+    else:
+        return b"HTTP/1.1 400 Bad Request\r\n\r\n"
 
 if(len(sys.argv) < 2):
     exit(1)
@@ -19,12 +50,29 @@ s.listen(0)
 
 msg = "here you go\r\n"
 while True:
-    conn, addr = s.accept()
-    data = conn.recv(4096)
-    print(data)
-    if not data:
-        conn.close()
-    if(conn.sendall(msg.encode()) == None):
-        conn.close()
+    try:
+        #accepts port
+        client, addr = s.accept()
+        #receives data
+        data = client.recv(4096)
+        #parse to get method
+        list_data = data.decode().split(" ", 1)
+        #handle method
+        if(list_data[0] == "GET"):
+    
+            send_msg = handleGET(list_data)
+        elif(list_data[0] == "POST"):
+            send_msg = handlePOST()
+        else:
+            if(client.sendall(b"HTTP/1.1 405 Method Not Allowed\r\n\r\n") == None):
+                client.close()
 
-s.close()
+        #if not data:
+        #   client.close()
+
+        #send message
+        if(client.sendall(send_msg) == None):
+            client.close()
+    except KeyboardInterrupt:
+        s.close()
+        sys.exit(0)
